f55c44c8c27ab10573c81a4aedffb8027a6db0be
Deepanshu Meemroth <deepanshu6479@gmail.com>
2024-08-23 08:52:45 +0530

    Lecture 1

---
+++ app.js
@@ -0,0 +1,15 @@
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// const heading = React.createElement("h1", {id:"heading"}, "Hello React")
+// root.render(heading)
+const parent = React.createElement("div", {id:"parent"}, 
+[React.createElement("div", {id:"child1"}, 
+[React.createElement("h1", {}, "Heading 1"),
+React.createElement("h1", {}, "Heading 2")]
+),
+React.createElement("div", {id:"child2"},
+[React.createElement("h1", {}, "Heading 1"),
+React.createElement("h1", {}, "Heading 2")]
+)]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
